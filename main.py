2b48bfb81100369023f1cac0449c1b0b09d0313c
ChetanKamatagi <kamatagichetan8@gmail.com>
2025-11-21 12:08:18 +0530

    Cleared a bug in main file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
 print("DEBUG TOKEN:", os.getenv("TELEGRAM_BOT_TOKEN"))
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 
 print("Got the Telegram token", TOKEN)
